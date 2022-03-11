4bfbc8c7dc5815fdb8b1e969fd94b0cf21e51256
Per von Zweigbergk <per.von.zweigbergk@cag.se>
2022-03-11 23:08:35 +0100

    Revert "Make Resource properties nullable"
    This reverts commit f89ea1ef87562954463f3e920613400bb59f2cfa.

--- PmpApiClient/Resource.cs
+++ PmpApiClient/Resource.cs
@@ -3,19 +3,27 @@ using System.Text.Json.Serialization;
 namespace PmpApiClient;
 
 public class Resource {
+    public Resource(string description, string name, string id, string type, int noOfAccounts) {
+        Description = description;
+        Name = name;
+        Id = id;
+        Type = type;
+        NoOfAccounts = noOfAccounts;
+    }
+
     [JsonPropertyName("RESOURCE DESCRIPTION")]
-    public string? Description { get; }
+    public string Description { get; }
 
     [JsonPropertyName("RESOURCE NAME")]
-    public string? Name { get; }
+    public string Name { get; }
 
     [JsonPropertyName("RESOURCE ID")]
-    public string? Id { get; }
+    public string Id { get; }
 
     [JsonPropertyName("RESOURCE TYPE")]
-    public string? Type { get; }
+    public string Type { get; }
 
     [JsonPropertyName("NOOFACCOUNTS")]
     [JsonNumberHandling(JsonNumberHandling.AllowReadingFromString)]
-    public int? NoOfAccounts { get; }
+    public int NoOfAccounts { get; }
 }
