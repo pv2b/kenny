6171eb4f3a0ea73d42da0bb0af53ee5557a39089
Per von Zweigbergk <pvz@pvz.pp.se>
2022-03-11 22:01:56 +0100

    Nest ApiOperation and ApiResult inside ApiResponse

--- PmpApiClient/ApiResponse.cs
+++ PmpApiClient/ApiResponse.cs
@@ -1,11 +1,48 @@
-using System.Text.Json.Serialization;
+﻿using System.Text.Json.Serialization;
 
 namespace PmpApiClient;
+
 public class ApiResponse<T>
 {
-    public ApiResponse(ApiOperation<T> operation) {
+    public class ApiOperation
+    {
+        public ApiOperation(string name, ApiResult result, int totalRows, T details) {
+            Name = name;
+            Result = result;
+            TotalRows = totalRows;
+            Details = details;
+        }
+
+        [JsonPropertyName("name")]
+        public string Name { get; }
+
+        [JsonPropertyName("result")]
+        public ApiResult Result { get; }
+
+        [JsonPropertyName("totalRows")]
+        public int TotalRows { get; }
+
+        [JsonPropertyName("Details")]
+        public T Details { get; }
+    }
+
+    public class ApiResult
+    {
+        public ApiResult(string status, string message) {
+            Status = status;
+            Message = message;
+        }
+
+        [JsonPropertyName("status")]
+        public string Status { get; }
+
+        [JsonPropertyName("message")]
+        public string Message { get; }
+    }
+
+    public ApiResponse(ApiOperation operation) {
         Operation = operation;
     }
     [JsonPropertyName("operation")]
-    public ApiOperation<T> Operation { get; }
+    public ApiOperation Operation { get; }
 }
