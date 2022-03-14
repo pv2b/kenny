6135bdcb48b4962245621a2bfe21f72e41b582fb
Per von Zweigbergk <per.von.zweigbergk@cag.se>
2022-03-14 22:04:39 +0100

    Added dynamic credenial stubs

--- RoyalJson/RoyalJsonDynamicCredential.cs
+++ RoyalJson/RoyalJsonDynamicCredential.cs
@@ -1,5 +1,9 @@
 ﻿namespace RoyalJson;
 public class RoyalJsonDynamicCredential
 {
-
+    public string? Username;
+    public string? Password;
+    public string? KeyFilePath;
+    public string? KeyFileContent;
+    public string? KeyFilePassphrase;
 }
