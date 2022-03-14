155234ae9ed920b9f75625be65f6ed97f804ce91
Per von Zweigbergk <pvz@pvz.pp.se>
2022-03-14 22:22:59 +0100

    Kill RoyalJson classlib - no need for unneccessary complexity

--- RoyalJson/RoyalJsonDynamicCredential.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RoyalJson;
-public class RoyalJsonDynamicCredential
-{
-    public string? Username;
-    public string? Password;
-    public string? KeyFilePath;
-    public string? KeyFileContent;
-    public string? KeyFilePassphrase;
-}
