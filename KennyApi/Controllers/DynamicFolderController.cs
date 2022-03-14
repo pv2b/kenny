8a76ca95ba04aa1b8df1e0efedaac1b378b9fc0f
Per von Zweigbergk <pvz@pvz.pp.se>
2022-03-14 23:08:19 +0100

    First stab at basic functionality

--- KennyApi/Controllers/DynamicFolderController.cs
+++ KennyApi/Controllers/DynamicFolderController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using PmpApiClient;
 
 namespace KennyApi.Controllers;
 
@@ -13,10 +14,43 @@ public class DynamicFolderController : ControllerBase
         _logger = logger;
     }
 
+    private Object makeRoyalJsonConnectionObject(Resource resource, ResourceAccountList resourceDetails, ResourceAccountList.Account account) {
+        return new {
+            Type="TerminalConnection",
+            Name=$"{resource.Name} ({account.Name})",
+            ComputerName=resourceDetails.DnsName,
+            CustomField1=resource.Id,
+            Path="Connections",
+            CredentialID=$"PmpCred_{account.Id}",
+        };
+    }
+
+    private Object makeRoyalJsonCredentialObject(Resource resource, ResourceAccountList resourceDetails, ResourceAccountList.Account account) {
+        return new {
+            Type="DynamicCredential",
+            Name=$"PMP credential for {resource.Name} ({account.Name})",
+            Id=$"PmpCred_{account.Id}",
+            Username=account.Name,
+            Path="Credentials",
+            TerminalConnectionType="SSH"
+        };
+    }
+
     [HttpGet(Name = "GetDynamicFolder")]
-    public Object Get(string apiUser)
+    public async Task<Object> Get(string apiUser)
     {
         var pmpApi = PmpApiClientStore.GetClient(apiUser);
-        throw new NotImplementedException();
+        IEnumerable<(Resource, Task<ResourceAccountList>)> resources = await pmpApi.GetAllResourceAccountListAsync();
+        var objects = new List<Object>();
+        foreach (var (resource, resourceAccountListTask) in resources) {
+            var resourceDetails = await resourceAccountListTask;
+            foreach (var account in resourceDetails.Accounts) {
+                objects.Add(makeRoyalJsonConnectionObject(resource, resourceDetails, account));
+                objects.Add(makeRoyalJsonCredentialObject(resource, resourceDetails, account));
+            }
+        }
+        return new {
+            Objects = objects
+        };
     }
 }
