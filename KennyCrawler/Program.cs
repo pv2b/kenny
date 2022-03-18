9cc5c95a5b1e72c8d3378bc4561dc33d8287368e
Per von Zweigbergk <per.von.zweigbergk@cag.se>
2022-03-18 13:14:58 +0100

    Initial KennyCrawler implementation

---
+++ KennyCrawler/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Text.Json;
+using PmpApiClient;
+
+var apiKeyringPath = Path.Join(AppContext.BaseDirectory, "ApiKeyring.json");
+var apiKeyring = new ApiKeyring(apiKeyringPath);
+
+var collectionNames = apiKeyring.GetCollectionNames();
+
+foreach (string collection in collectionNames) {
+    Console.WriteLine("Crawling collection {collection}...");
+    var pmpApiClient = apiKeyring.CreateApiClient(collection);
+    var resourceFilePath = Path.Join(AppContext.BaseDirectory, $"Resources-{collection}.json");
+    var resourceFileTempPath = Path.Join(AppContext.BaseDirectory, $"Resources-{collection}.json.tmp");
+
+    List<Resource> resources = new List<Resource>();
+    await foreach (var resource in pmpApiClient.GetAllResourcesAsync()) {
+        resources.Add(resource);
+    }
+    using (FileStream fs = File.Open(resourceFileTempPath, FileMode.Create, FileAccess.Write, FileShare.None)) {
+        JsonSerializer.Serialize<List<Resource>>(fs, resources);
+    }
+    File.Move(resourceFileTempPath, resourceFilePath);
+}
+Console.WriteLine("Done!");
