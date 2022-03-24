edd45a764add4cd08b7dc79faeb5de1242b02ef3
Per von Zweigbergk <pvz@pvz.pp.se>
2022-03-24 19:05:44 +0100

    Improved error reporting

--- PmpApiClient/BasePmpApiClient.cs
+++ PmpApiClient/BasePmpApiClient.cs
@@ -3,12 +3,12 @@ namespace PmpApiClient;
 using System;
 
 public abstract class BasePmpApiClient {
-    private T HandleApiResponse<T>(ApiResponse<T>? response) {
+    private T HandleApiResponse<T>(ApiResponse<T>? response, string operationDescription) {
         if (response == null) {
-            throw new Exception("Empty API response");
+            throw new Exception($"Error when {operationDescription}: Empty API response");
         }
         if (!response.Operation.Result.Status.Equals("success", StringComparison.CurrentCultureIgnoreCase)) {
-            throw new Exception(response.Operation.Result.Message);
+            throw new Exception($"Error when {operationDescription}: {response.Operation.Result.Message}");
         }
         return response.Operation.Details;
     }
@@ -16,13 +16,13 @@ public abstract class BasePmpApiClient {
     public abstract Task<ApiResponse<IEnumerable<ResourceSummary>>?> GetAllResourceSummaryApiResponseAsync();
     public async Task<IEnumerable<ResourceSummary>> GetAllResourceSummaryAsync() {
         var response = await GetAllResourceSummaryApiResponseAsync();
-        return HandleApiResponse<IEnumerable<ResourceSummary>>(response);
+        return HandleApiResponse<IEnumerable<ResourceSummary>>(response, $"getting all resource summaries");
     }
 
     public abstract Task<ApiResponse<AssociatedGroupContainer>?> GetResourceAssociatedGroupsApiResponseAsync(string resourceId);
     public async Task<IEnumerable<ResourceGroupSummary>> GetResourceAssociatedGroupsAsync(string resourceId) {
         var response = await GetResourceAssociatedGroupsApiResponseAsync(resourceId);
-        return HandleApiResponse<AssociatedGroupContainer>(response)?.Groups ?? Enumerable.Empty<ResourceGroupSummary>();
+        return HandleApiResponse<AssociatedGroupContainer>(response, $"getting resource associated groups for resource {resourceId}")?.Groups ?? Enumerable.Empty<ResourceGroupSummary>();
     }
     public Task<IEnumerable<ResourceGroupSummary>> GetResourceAssociatedGroupsAsync(ResourceSummary resourceSummary) {
         if (resourceSummary.Id == null) {
@@ -34,7 +34,7 @@ public abstract class BasePmpApiClient {
     public abstract Task<ApiResponse<ResourceDetails>?> GetResourceDetailsApiResponseAsync(string resourceId);
     public async Task<ResourceDetails> GetResourceDetailsAsync(string resourceId) {
         var response = await GetResourceDetailsApiResponseAsync(resourceId);
-        return HandleApiResponse<ResourceDetails>(response);
+        return HandleApiResponse<ResourceDetails>(response, $"getting resource details for resource {resourceId}");
     }
     public Task<ResourceDetails> GetResourceDetailsAsync(ResourceSummary resourceSummary) {
         if (resourceSummary.Id == null) {
@@ -63,7 +63,7 @@ public abstract class BasePmpApiClient {
         var passwordRequestDetails = new PasswordRequestDetails(reason, ticketId);
         var request = new ApiRequest<PasswordRequestDetails>(passwordRequestDetails);
         var response = await GetAccountPasswordApiResponseAsync(resourceId, accountId, request);
-        return HandleApiResponse<AccountPassword>(response);
+        return HandleApiResponse<AccountPassword>(response, @"getting account password for resource {resourceId} and account {accountId}");
     }
     public Task<AccountPassword> GetAccountPasswordAsync(ResourceSummary resource, ResourceDetails.Account account, string? reason = null, string? ticketId = null) {
         if (resource.Id == null) {
