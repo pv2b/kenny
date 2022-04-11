de3af1358b9ab59c2c53b3d627abbaf775b3cb42
Per von Zweigbergk <pvz@pvz.pp.se>
2022-04-11 17:16:59 +0200

    Also get AD Groups from SQL Server

--- PmpSqlClient/PmpSqlClient.cs
+++ PmpSqlClient/PmpSqlClient.cs
@@ -17,18 +17,40 @@ public class PmpSqlClient
             {  
                 command.Connection = connection; 
                 command.CommandType = DT.CommandType.Text; 
-                command.CommandText = "SELECT GROUPID, PARENT_ID, GROUPNAME, GROUPDESC FROM [dbo].[Ptrx_ResourceGroup]";
+                command.CommandText = @"SELECT A.GROUPID, A.PARENT_ID, A.GROUPNAME, A.GROUPDESC, B.ACCESSTYPE, C.ADSPATH
+                    FROM [dbo].[Ptrx_ResourceGroup] AS A
+                    INNER JOIN [PassTrix].[dbo].[Ptrx_ResGrpUserGroup] AS B ON A.[GROUPID] = B.[GROUPID]
+                    INNER JOIN [PassTrix].[dbo].[Ptrx_ActiveDirectoryGroupSyncDetails] AS C ON B.[USERGROUPID] = C.[GROUP_ID]
+                    ORDER BY A.GROUPID;
+                    ";
 
                 QC.SqlDataReader reader = await command.ExecuteReaderAsync();  
 
+                ResourceGroup? current = null;
+
                 while (await reader.ReadAsync()) {  
-                    yield return new ResourceGroup(
-                        id: reader.GetInt64(0),
-                        parentId: reader.GetInt64(1),
-                        name: reader.GetString(2),
-                        description: reader.GetString(3)
-                    );
+                    long groupId = reader.GetInt64(0);
+                    if (groupId != current?.Id) {
+                        if (current != null) yield return current;
+                        current = new ResourceGroup(
+                            id: groupId,
+                            parentId: reader.GetInt64(1),
+                            name: reader.GetString(2),
+                            description: reader.GetString(3)
+                        );
+                    }
+                    switch (reader.GetString(4) /* accesstype */) {
+                        case "readonly":
+                        case "readwrite":
+                        case "complete":
+                            current.AllowGroups.Add(reader.GetString(5) /* adstype */);
+                            break;
+
+                        default:
+                            break;
+                    }
                 } 
+                if (current != null) yield return current;
             }
         }
     }
--- PmpSqlClient/ResourceGroup.cs
+++ PmpSqlClient/ResourceGroup.cs
@@ -5,10 +5,12 @@ public class ResourceGroup {
     public long ParentId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+    public List<string> AllowGroups { get; set; }
     public ResourceGroup(long id, long parentId, string name, string description) {
         Id = id;
         ParentId = parentId;
         Name = name;
         Description = description;
+        AllowGroups = new List<string>();
     }
 }
